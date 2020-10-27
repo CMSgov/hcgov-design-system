9b38c328490a7a240d4e408055e573e442584654
bernardwang <bw.vbnm@gmail.com>
2020-10-27 16:07:39 -0500

    Fix rootPath config

--- cmsds.config.js
+++ cmsds.config.js
@@ -4,7 +4,7 @@ module.exports = {
   // The relative path to the directory containing the doc site `src`. The doc site build files will be saved here under "dist".
   docsDir: './docs',
   // The URL root path for the published docs site. I.e. if your docs site is hosted at www.domain.com/design/ your rootPath would be `design`. `rootPath` is only used when building for production.
-  rootPath: '',
+  rootPath: 'hcgov-design-system',
 
   // Name of the design system. This replaces the {{name}} template in documentation content.
   name: 'HealthCare.gov Design System',
