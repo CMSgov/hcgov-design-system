577c1c2f3305a0038e8a407669077a7b34c7db45
line47 <scott.weber@adhocteam.us>
2020-11-11 12:11:20 -0500

    testing lint

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,14 @@ import { translate } from 'react-i18next';
 const _Footer = function (props) {
   const classes = classnames(
     'hc-c-footer ds-u-fill--gray-lightest ds-u-padding-y--5',
-    props.className
+    props.classNamemodule.exports = {
+      meta: { ... },
+      create: function(context) {
+          // declare the state of the rule
+          return {
+              ReturnStatement: function(node) {
+                  // at a ReturnStatement node while going down
+              },
   );
 
   return (
