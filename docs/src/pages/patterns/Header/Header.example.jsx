23cccce274d0429615d1524557664681b653507f
line47 <scott.weber@adhocteam.us>
2021-09-09 09:29:14 -0400

    Show all variations of the header on the page

--- docs/src/pages/patterns/Header/Header.example.jsx
+++ docs/src/pages/patterns/Header/Header.example.jsx
@@ -2,14 +2,6 @@ import { ChoiceList, Header } from '@design-system';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const variationChoices = [
-  { value: 'home', label: 'Home' },
-  { value: 'product', label: 'Product' },
-  { value: 'product-de', label: 'Product (DE)' },
-  { value: 'logged-in', label: 'Logged-in' },
-  { value: 'logged-in-de', label: 'Logged-in (DE)' },
-];
-
 const localeChoices = [
   { value: 'en', label: 'English' },
   { value: 'es', label: 'Español' },
@@ -21,17 +13,11 @@ class HeaderExample extends React.PureComponent {
 
     this.state = {
       locale: 'en',
-      variation: 'logged-in',
     };
   }
 
   renderHeaderToggles() {
-    const { locale, variation } = this.state;
-
-    const variations = variationChoices.map((choice) => ({
-      ...choice,
-      checked: choice.value === variation,
-    }));
+    const { locale } = this.state;
     const locales = localeChoices.map((choice) => ({
       ...choice,
       checked: choice.value === locale,
@@ -39,13 +25,6 @@ class HeaderExample extends React.PureComponent {
 
     return (
       <div className="ds-u-margin-top--4">
-        <ChoiceList
-          label="Change variation"
-          name="variation"
-          choices={variations}
-          onChange={(event) => this.setState({ variation: event.target.value })}
-          type="radio"
-        />
         <ChoiceList
           label="Change language"
           name="locale"
@@ -58,28 +37,32 @@ class HeaderExample extends React.PureComponent {
   }
 
   render() {
-    const { locale, variation } = this.state;
-    const headerProps = {
-      home: {},
-      product: { subhead: 'Tax Tools', subpath: 'tax-tool/' },
-      'product-de': {
-        subhead: 'Verify your identity',
-        deConsumer: true,
-        deBrokerName: 'Acme Co.',
-      },
-      'logged-in': { loggedIn: true, firstName: 'Maximiliano-Longname' },
-      'logged-in-de': {
-        loggedIn: true,
-        deConsumer: true,
-        deBrokerName: 'Acme Co.',
-      },
-    };
+    const { locale } = this.state;
     const wrapperClassNames = 'ds-u-padding--1';
 
     return (
       // Add min-height so the options don't get cut off when switching to product-de
       <div style={{ minHeight: 679 }}>
-        <Header {...headerProps[variation]} initialLanguage={locale} />
+        <h6 className="preview__label">Header home</h6>
+        <Header initialLanguage={locale} />
+        <h6 className="preview__label">Header product</h6>
+        <Header subhead="Tax Tools" subpath="tax-tool/" initialLanguage={locale} />
+        <h6 className="preview__label">Header logged in</h6>
+        <Header loggedIn="true" firstName="Maximiliano-Longname" initialLanguage={locale} />
+        <h6 className="preview__label">(Direct Enrollment) Header product </h6>
+        <Header
+          subhead="Verify your identity"
+          deConsumer="true"
+          deBrokerName="Acme Co."
+          initialLanguage={locale}
+        />
+        <h6 className="preview__label">(Direct Enrollment) Header logged in</h6>
+        <Header
+          loggedIn="true"
+          deConsumer="true"
+          deBrokerName="Acme Co."
+          initialLanguage={locale}
+        />
         <div className={wrapperClassNames}>{this.renderHeaderToggles()}</div>
       </div>
     );
